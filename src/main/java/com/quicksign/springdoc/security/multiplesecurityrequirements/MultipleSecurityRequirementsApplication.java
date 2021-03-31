cdc3d21f3b3b184beece04391f34587e309d9a27
Arnaud Jeansen <arnaud.jeansen@quicksign.com>
2021-03-31 09:04:30 +0200

    Create spring web starter from spring initializr

---
+++ src/main/java/com/quicksign/springdoc/security/multiplesecurityrequirements/MultipleSecurityRequirementsApplication.java
@@ -0,0 +1,13 @@
+package com.quicksign.springdoc.security.multiplesecurityrequirements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleSecurityRequirementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleSecurityRequirementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quicksign/springdoc/security/multiplesecurityrequirements/MultipleSecurityRequirementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quicksign.springdoc.security.multiplesecurityrequirements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleSecurityRequirementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
