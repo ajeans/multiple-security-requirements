e4ef460542b3823f9af632e5194350d26d54201e
Arnaud Jeansen <arnaud.jeansen@quicksign.com>
2021-03-31 14:58:57 +0200

    Add springdoc and some basic openapi info to the spec

---
+++ src/main/java/com/quicksign/springdoc/security/multiplesecurityrequirements/OpenApiConfiguration.java
@@ -0,0 +1,13 @@
+package com.quicksign.springdoc.security.multiplesecurityrequirements;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+    info =
+    @Info(
+        title = "Reproducer",
+        version = "0.1.0-SNAPSHOT",
+        description = "Test multiple security requirements"))
+public class OpenApiConfiguration {
+}
