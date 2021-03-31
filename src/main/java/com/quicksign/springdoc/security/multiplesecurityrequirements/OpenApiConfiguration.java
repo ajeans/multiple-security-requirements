f28cdb16e3c8e5412568f3ad21f7c66397310dad
Arnaud Jeansen <arnaud.jeansen@quicksign.com>
2021-03-31 15:02:45 +0200

    Add two security requirements and prove that they do not appear

--- src/main/java/com/quicksign/springdoc/security/multiplesecurityrequirements/OpenApiConfiguration.java
+++ src/main/java/com/quicksign/springdoc/security/multiplesecurityrequirements/OpenApiConfiguration.java
@@ -1,7 +1,10 @@
 package com.quicksign.springdoc.security.multiplesecurityrequirements;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 
 @OpenAPIDefinition(
     info =
@@ -9,5 +12,21 @@ import io.swagger.v3.oas.annotations.info.Info;
         title = "Reproducer",
         version = "0.1.0-SNAPSHOT",
         description = "Test multiple security requirements"))
+/*
+Doesn't work: "components" is empty
+@SecuritySchemes({
+    @SecurityScheme(name = "basicAuth", type = SecuritySchemeType.HTTP, scheme = "basic"),
+    @SecurityScheme(
+        name = "bearerToken",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT")
+})*/
+// Works for one scheme: "components" has 1 "securitySchemes"
+@SecurityScheme(
+    name = "bearerToken",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT")
 public class OpenApiConfiguration {
 }
